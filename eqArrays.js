60c3202c5dbc3cb8e03b649ebe8a9a730346b4c7
Abdulkabir Ojularim <abdulkabirojulari@gmail.com>
2022-01-03 01:46:33 +0000

    complete the lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function (actual, expected) {
-    if (actual !== expected) {
-        return `Assertion failed: ${actual} !== ${expected}`;
-    }
-    else {
-        return `Assertion Passed: ${actual} === ${expected}`;
-    }
-
-};
+const assertEqual = require('./assertEqual');
 
 function eqArray(a, b){
     const container =[];
@@ -31,5 +23,4 @@ function eqArray(a, b){
     }
 }
 
-console.log(assertEqual(eqArray([2,3,4], [2,5,4]), true));
-console.log(assertEqual(eqArray([2,3,4], [2,3,4]), true));
+module.exports = eqArray;
--- head.js
+++ head.js
@@ -1,8 +1,7 @@
-const assertEqual = require('./assertEqual')
 
 function Head(element = []) { 
     return element[0];
 }
 
-console.log (assertEqual(Head([5,6,7]), 5));
-console.log (assertEqual(Head(['Hello','Lighthouse','Labs']), 'Hello'));
+module.exports = Head;
+
